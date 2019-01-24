8456b5c22022f7816ebcf29cffb9243252413393
1181888200 <1181888200@qq.com>
2019-01-24 14:40:24 +0800

    mybatis通过AOP实现主从库读写分离

---
+++ src/main/java/com/lwl/MybatisWrAopApplication.java
@@ -0,0 +1,14 @@
+package com.lwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisWrAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisWrAopApplication.class, args);
+	}
+
+}
+
